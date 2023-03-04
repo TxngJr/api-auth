4e80c22944cbc8393dd6786e3524af513dbfafc5
Soraaot Channon <116389026+TxngJr@users.noreply.github.com>
2023-03-04 17:02:40 +0700

    Update database.js

--- config/database.js
+++ config/database.js
@@ -1,7 +1,5 @@
 const mongoose =require("mongoose");
 
-const {MONGO_URI} = process.env;
-
 exports.connect = () => {
     mongoose.set("strictQuery", false);
     mongoose.connect(process.env.MONGO_URI)
